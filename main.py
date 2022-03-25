2247600d5e32b1ebc1ba4f4b027ad6e21bb4f049
austincarreno <austinraycarreno96@gmail.com>
2022-03-25 09:44:54 -0400

    Test for OS system. We need the filepath for Windows systems

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from PyGUI import PyGUI
 import os
+import platform
 
 
 def main():
-    os.system("clasp ~/try1.cnf -n 4 > TEST.txt")
+    if platform.system() == "Darwin":
+        os.system("clasp ~/try1.cnf -n 4 > TEST.txt")
+    else:
+        print("Need file path for CLASP")
+
     myPyGUI = PyGUI()
 
 if __name__ == "__main__":
