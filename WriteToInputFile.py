9fbb70d51c56fe4d89d008328e90b6799fd238c9
austincarreno <austinraycarreno96@gmail.com>
2022-03-28 15:40:33 -0400

    The program is now creating the correct file to feed to clasp.

--- WriteToInputFile.py
+++
@@ -1,5 +0,0 @@
-def writeToTheFile(attribute_dict, statement_count):
-    test_file = open("CNF.txt", "w")
-    test_file.write('c\n' + 'c This is a test file for CLASP\n' + 'c\n' + 'p cnf ' + str(attribute_dict['count']) +
-                    ' ' + str(statement_count) + ' \n1 0\n3 0\n')
-    return test_file
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PyGUI import PyGUI
 import os
 import platform
-import WriteToInputFile
 import Objects
+import re as regular_expression
 
 # TODO: Things Needed to Be Done
 #       - Update where the file we're opening to be a selected file from the GUI rather than my test file
@@ -18,9 +18,39 @@ import Objects
 #       - Take hard constraint and make feasible objects, from there apply penalty and possibilistic logic on those
 #         objects
 
+
 def main():
     statement_count = 0
+    # Holding all necessary words and logic for output to user
     input_dict = {
+        'attributes': {
+            'words': [],
+            'numbers': []
+        },
+        'hard_constraints': {
+            'stmts': [],
+            'numbers': [],
+            'clasp_ready_stmts': [],
+            'constraint_statement': ''
+        },
+        'penalty_logic': {
+            'logic_stmts': [],
+            'penalty': [],
+            'stmts_as_numbers': [],
+            'clasp_ready_stmts': []
+        },
+        'possibilistic_logic': {
+            'logic_stmts': [],
+            'tolerance': [],
+            'stmts_as_numbers': [],
+            'clasp_ready_stmts': []
+        },
+        'qualitative_choice_logic': [],
+        'clasp_ready_stmts': []
+    }
+
+    # Will hold same values as input_dict, but as numbers for clasp
+    number_dict = {
         'attributes': [],
         'hard_constraints': [],
         'penalty_logic': {
@@ -36,15 +66,55 @@ def main():
 
     # Open Files
     attr = Objects.Attributes('Attributes.txt')
-    input_dict['attributes'] = attr.readAttributes()
+    input_dict['attributes']['words'] = attr.readAttributes()
 
+
+    # Opening hard constraints file and manipulating string inside
     hard_constraint_file = open('HardConstraintInput.txt', 'r')
     hard_constraint = hard_constraint_file.read()
-    input_dict['hard_constraints'] = hard_constraint.split('\n')
+    input_dict['hard_constraints']['stmts'] = hard_constraint.split('\n')
+    ConvertConstraintsToNumbers(input_dict)
 
+    for index in range(len(input_dict['hard_constraints']['numbers'])):
+        for logic in input_dict['hard_constraints']['numbers'][index]:
+            if logic == 'NOT':
+                input_dict['hard_constraints']['numbers'][index].remove(logic)
+
+    for outer_index in range(len(input_dict['hard_constraints']['numbers'])):
+        string = ''
+        for index in range(len(input_dict['hard_constraints']['numbers'][outer_index])):
+            string += str(input_dict['hard_constraints']['numbers'][outer_index][index])
+        string += ' 0'
+        input_dict['hard_constraints']['clasp_ready_stmts'].append(string)
+        input_dict['hard_constraints']['constraint_statement'] += \
+            input_dict['hard_constraints']['clasp_ready_stmts'][outer_index] + '\n'
+    input_dict['hard_constraints']['clasp_ready_stmts'].remove(input_dict['hard_constraints']['clasp_ready_stmts']
+                                                               [len(input_dict['hard_constraints']
+                                                                    ['clasp_ready_stmts']) - 1])
+
+
+    # Breaking down logic files to get input ready for clasp
     breakDownLogicFile('Logic.txt', input_dict)
-    logicStatementConvertToNumbers(input_dict)
 
+    # Converting words to numbers for clasp
+    statement_count = logicStatementConvertToNumbers(input_dict, statement_count)
+
+    # Removing 'NOT' from logic statements
+    for index in range(len(input_dict['penalty_logic']['stmts_as_numbers'])):
+        for logic in input_dict['penalty_logic']['stmts_as_numbers'][index]:
+            if logic == 'NOT':
+                input_dict['penalty_logic']['stmts_as_numbers'][index].remove(logic)
+
+
+    for outer_index in range(len(input_dict['penalty_logic']['stmts_as_numbers'])):
+        string = ''
+        for index in range(len(input_dict['penalty_logic']['stmts_as_numbers'][outer_index])):
+            string += str(input_dict['penalty_logic']['stmts_as_numbers'][outer_index][index])
+        string += ' 0'
+        input_dict['penalty_logic']['clasp_ready_stmts'].append(string)
+
+    # TODO: Remove when ready
+# ----------------------------------------------------------------------------------------------------------------------
     # attributes_text_file = open('Attributes.txt', 'r')
     # attributes_listed = attributes_text_file.read()
     # attributes_split_by_type = attributes_listed.split('\n')
@@ -55,7 +125,7 @@ def main():
     # for index in range(len(attributes_split_from_type)):
     #     input_dict['attributes']['Type'].append(attributes_split_from_type[index][0])
     #     input_dict['attributes']['Attribute_List'].append(attributes_split_from_type[index][1])
-
+# ----------------------------------------------------------------------------------------------------------------------
 
     # input_dict['attributes']['Attribute_List'][0].split(', ')
 
@@ -65,17 +135,21 @@ def main():
     #     input_dict['attributes'].append()
 
 
+    # Converting attributes to numbers as needed
+    input_dict['attributes']['numbers'] = convertAttributesToNumbers(input_dict['attributes']['words'])
 
-    attribute_dict = convertAttributesToNumbers(input_dict['attributes'])
-    WriteToInputFile.writeToTheFile(attribute_dict, len(input_dict['hard_constraints']))
+    # Writing to file that will be sent to clasp
+    writeToTheFile(input_dict)
+
+    # Naming file that will be sent to clasp
     test2_file = 'CNF.txt'
 
     if platform.system() == "Darwin":
-        os.system("clasp " + test2_file + " -n 10 > TEST.txt")
+        os.system("clasp " + test2_file + " -n 0 > CLASPOutput.txt")
     else:
-        os.system("clasp " + test2_file + " -n 2 > TEST.txt")
+        os.system("clasp " + test2_file + " -n 2 > CLASPOutput.txt")
 
-    # myPyGUI = PyGUI()
+    # myPyGUI = PyGUI(input_dict)
 # ----------------------------------------------------------------------------------------------------------------------
 
 
@@ -109,21 +183,73 @@ def convertAttributesToNumbers(attributes):
                 attribute_number_list.append(4)
             case "ice-cream":
                 attribute_number_list.append(-4)
-        attribute_count += 1
 
-    attribute_dict = {
-        'count': attribute_count,
-        'list': attribute_number_list
-    }
-
-    return attribute_dict
+    return attribute_number_list
 # ----------------------------------------------------------------------------------------------------------------------
 
 def logicStatementConvertToNumbers(input_dict):
-    test = []
-    test2 = []
+    statement_split_list = []
     for index in range(len(input_dict['penalty_logic']['logic_stmts'])):
-        test.append(input_dict['penalty_logic']['logic_stmts'][index].split(' '))
+        statement_split_list.append(input_dict['penalty_logic']['logic_stmts'][index].split(' '))
+
+    for outer_index in range(len(input_dict['penalty_logic']['logic_stmts'])):
+
+        for inner_index in range(len(statement_split_list[outer_index])):
+            match statement_split_list[outer_index][inner_index]:
+                case "salad":
+                    salad = 1
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -salad
+                    else:
+                        statement_split_list[outer_index][inner_index] = salad
+                case "soup":
+                    soup = -1
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -soup
+                    else:
+                        statement_split_list[outer_index][inner_index] = soup
+                case "fish":
+                    fish = 2
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -fish
+                    else:
+                        statement_split_list[outer_index][inner_index] = fish
+                case "beef":
+                    beef = -2
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -beef
+                    else:
+                        statement_split_list[outer_index][inner_index] = beef
+                case "wine":
+                    wine = 3
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -wine
+                    else:
+                        statement_split_list[outer_index][inner_index] = wine
+                case "beer":
+                    beer = -3
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -beer
+                    else:
+                        statement_split_list[outer_index][inner_index] = beer
+                case "cake":
+                    cake = 4
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -cake
+                    else:
+                        statement_split_list[outer_index][inner_index] = cake
+                case "ice-cream":
+                    ice_cream = -4
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -ice_cream
+                    else:
+                        statement_split_list[outer_index][inner_index] = ice_cream
+                case "OR":
+                    statement_split_list[outer_index][inner_index] = ' '
+                case "AND":
+                    statement_split_list[outer_index][inner_index] = ' 0\n'
+    input_dict['penalty_logic']['stmts_as_numbers'] = statement_split_list
+
 # ----------------------------------------------------------------------------------------------------------------------
 
 
@@ -182,4 +308,83 @@ def breakDownLogicFile(file_name, input_dict):
 # ----------------------------------------------------------------------------------------------------------------------
 
 
+def writeToTheFile(input_dict):
+    test_file = open("CNF.txt", "w")
+    test_file.write('c\n' + 'c This is a test file for CLASP\n' + 'c\n' + 'p cnf '
+                    + str(len(input_dict['attributes']['words'])) +
+                    ' ' + str(len(input_dict['hard_constraints']['numbers'])) + ' \n' +
+                    input_dict['hard_constraints']['constraint_statement'])
+    return test_file
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+def ConvertConstraintsToNumbers(input_dict):
+    statement_split_list = []
+    for index in range(len(input_dict['hard_constraints']['stmts'])):
+        statement_split_list.append(input_dict['hard_constraints']['stmts'][index].split(' '))
+    # print(statement_split_list)
+
+    for outer_index in range(len(statement_split_list)):
+
+        for inner_index in range(len(statement_split_list[outer_index])):
+            match statement_split_list[outer_index][inner_index]:
+                case "salad":
+                    salad = 1
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -salad
+                    else:
+                        statement_split_list[outer_index][inner_index] = salad
+                case "soup":
+                    soup = -1
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -soup
+                    else:
+                        statement_split_list[outer_index][inner_index] = soup
+                case "fish":
+                    fish = 2
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -fish
+                    else:
+                        statement_split_list[outer_index][inner_index] = fish
+                case "beef":
+                    beef = -2
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -beef
+                    else:
+                        statement_split_list[outer_index][inner_index] = beef
+                case "wine":
+                    wine = 3
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -wine
+                    else:
+                        statement_split_list[outer_index][inner_index] = wine
+                case "beer":
+                    beer = -3
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -beer
+                    else:
+                        statement_split_list[outer_index][inner_index] = beer
+                case "cake":
+                    cake = 4
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -cake
+                    else:
+                        statement_split_list[outer_index][inner_index] = cake
+                case "ice-cream":
+                    ice_cream = -4
+                    if statement_split_list[outer_index][inner_index - 1] == 'NOT':
+                        statement_split_list[outer_index][inner_index] = -ice_cream
+                    else:
+                        statement_split_list[outer_index][inner_index] = ice_cream
+                case "OR":
+                    # statement_count += 1
+                    statement_split_list[outer_index][inner_index] = ' '
+                case "AND":
+                    # statement_count += 1
+                    statement_split_list[outer_index][inner_index] = ' 0\n'
+    input_dict['hard_constraints']['numbers'] = statement_split_list
+    # return statement_count
+# ---------------------------------------------------------------------------------------------------------------------
+
 main()
+
